ac11ed10d89759fad2046e6fac118448e984fc4e
Martin Horn <martin.horn@knime.com>
2025-05-27 12:39:12 +0200

    AP-24414: WIP
    AP-24414 (Enable data area in workflow tool cell)

--- org.knime.buildworkflows/src/org/knime/buildworkflows/reader/WorkflowReaderNodeModel.java
+++ org.knime.buildworkflows/src/org/knime/buildworkflows/reader/WorkflowReaderNodeModel.java
@@ -252,6 +252,7 @@ public final class WorkflowReaderNodeModel extends AbstractPortObjectRepositoryN
 
     }
 
+    // TODO move (ticket number)
     public static WorkflowManager readWorkflow(final File wfFile, final ExecutionContext exec,
         final MessageBuilder messageBuilder) throws IOException, InvalidSettingsException, CanceledExecutionException,
         UnsupportedWorkflowVersionException, LockFailedException, KNIMEException {
@@ -279,6 +280,7 @@ public final class WorkflowReaderNodeModel extends AbstractPortObjectRepositoryN
         return loadResult.getWorkflowManager();
     }
 
+    // TOOD move (ticket number)
     @SuppressWarnings("resource")
     public static TempPathCloseable toLocalWorkflowDir(final FSPath path, final ItemVersion version)
         throws IOException {
